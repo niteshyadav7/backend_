07fec0c0bffdda42f51baf49d43e638791dc041d
niteshyadav7 <nitesh0709yadav@gmail.com>
2025-02-09 02:32:59 +0530

    dfrd

--- server.js
+++ server.js
@@ -62,12 +62,12 @@ app.delete("/api/tasks/:id", async (req, res) => {
 });
 
 // Serve frontend (React build files)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // Start the server
 app.listen(port, () => {
