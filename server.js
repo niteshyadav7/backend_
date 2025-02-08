7ca6a4bd035e894bb488b022d4d56b7f364105d4
niteshyadav7 <nitesh0709yadav@gmail.com>
2025-02-09 02:35:26 +0530

    erer

--- server.js
+++ server.js
@@ -61,13 +61,7 @@ app.delete("/api/tasks/:id", async (req, res) => {
   res.send("Task Deleted");
 });
 
-// Serve frontend (React build files)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+
 
 // Start the server
 app.listen(port, () => {
